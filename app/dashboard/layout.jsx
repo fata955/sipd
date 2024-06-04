42211837fa9d73eb9c2de300bd940ba4aa8c8923
unknown <fatahillah955@gmail.com>
2024-06-04 09:48:00 +0800

    first commit

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,18 @@
+import Sidebar from '../ui/dashboard/sidebar/sidebar'
+import Navbar from '../ui/dashboard/navbar/navbar'
+
+const layout = ({children}) => {
+  return (
+    <div>
+        <div>
+            <Sidebar />
+        </div>
+        <div>
+            <Navbar />
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default layout
